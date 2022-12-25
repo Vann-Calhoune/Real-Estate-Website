dd79b2b6af493220804b6232390416c615aa494c
Vann Calhoune <vanncalhoune@Vanns-Air.lan>
2022-12-24 19:15:53 -0600

    favicon & styling

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,12 @@ import '../styles/globals.css'
 import Navbar from "../components/Navbar"
 import Footer from '../components/Footer'
 import { Poppins } from '@next/font/google'
-import Contact from '../components/Contact';
+import Contact from '../components/Contact'
+import Head from 'next/head'
 
 
-const ashkash = Poppins({ 
+
+const poppins = Poppins({ 
   subsets: ['latin'],
   weight: '400'
 });
@@ -13,7 +15,16 @@ const ashkash = Poppins({
 function MyApp({ Component, pageProps }) {
  return (
  <>
- <main className={ashkash.className}>
+ <Head>
+  <title>We Buy Houses Cash Kansas City! | We Want To Solve Your Real Estate Problems!</title>
+  <meta name="description" content="We make the process of selling your home fast and easy. We serve Kansas City and the surrounding areas. Submit a contact form or reach out to us today."></meta>
+  <link rel="canonical" href="https://www.vannbuyshouses.com/" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+<link rel="manifest" href="/site.webmanifest"/>
+ </Head>
+ <main className={poppins.className}>
    <Contact />
     <Navbar />
     <Component {...pageProps} />
